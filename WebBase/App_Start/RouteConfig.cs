3f29ea83a4397bdac2cfb29efc0870ecda19601d
Vladimir Rõkovanov <vladimir.rokovanov@itcollege.ee>
2016-06-09 13:02:46 +0300

    vaated ei kasutata

--- WebBase/App_Start/RouteConfig.cs
+++ WebBase/App_Start/RouteConfig.cs
@@ -10,14 +10,15 @@ namespace WebBase
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
-        {
+        {/*
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                  "Default",
                 "{*url}",
                   new { controller = "Home", action = "Index" }
-             );
+             );*/
+            routes.MapPageRoute("Default", "{*anything}", "~/index.html");
         }
     }
 }
